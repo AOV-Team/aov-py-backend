3e496eb3ea4055763b0da08f825649041b3a37f1
Martin Ronquillo <ronquillo.m@gmail.com>
2016-11-25 19:28:28 -0700

    Profile patch unit tests

--- apps/account/tests/test_me_profile_view_set.py
+++ apps/account/tests/test_me_profile_view_set.py
@@ -51,3 +51,104 @@ class TestMeProfileViewSetGET(TestCase):
         request = client.get('/api/me/profile')
 
         self.assertEquals(request.status_code, 404)
+
+
+class TestMeProfileViewSetPATCH(TestCase):
+    """
+    Test PATCH /api/me/profile
+    """
+    def test_me_profile_view_set_patch_successful(self):
+        """
+        Test that we can update user's profile
+
+        :return: None
+        """
+        # Create test data
+        user = account_models.User.objects.create_user(email='mrtest@mypapaya.io', password='pass', username='aov_hov')
+        account_models.Profile.objects.create_or_update(user=user, bio='I am a tester.')
+
+        # Simulate auth
+        token = test_helpers.get_token_for_user(user)
+
+        # Get data from endpoint
+        client = APIClient()
+
+        payload = {
+            'bio': 'Foo'
+        }
+
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+
+        request = client.patch('/api/me/profile', data=payload, format='json')
+        result = request.data['result']
+
+        # Test return data
+        self.assertEquals(result['bio'], 'Foo')
+        self.assertEquals(result['cover_image'], '')
+        self.assertEquals(result['gear'], '')
+
+        # Check db entry too
+        updated_profile = account_models.Profile.objects.get(user=user)
+
+        self.assertEquals(updated_profile.bio, 'Foo')
+
+    def test_me_profile_view_set_patch_not_found(self):
+        """
+        Test that we get 404 if profile does not exist
+
+        :return: None
+        """
+        # Create test data
+        user = account_models.User.objects.create_user(email='mrtest@mypapaya.io', password='pass', username='aov_hov')
+
+        # Simulate auth
+        token = test_helpers.get_token_for_user(user)
+
+        # Get data from endpoint
+        client = APIClient()
+
+        payload = {
+            'bio': 'Foo'
+        }
+
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+
+        request = client.patch('/api/me/profile', data=payload, format='json')
+
+        self.assertEquals(request.status_code, 404)
+
+    def test_me_profile_view_set_patch_sanitize(self):
+        """
+        Test that we cannot change user
+
+        :return: None
+        """
+        # Create test data
+        user = account_models.User.objects.create_user(email='mrtest@mypapaya.io', password='pass', username='aov_hov')
+        profile1 = account_models.Profile.objects.create_or_update(user=user, bio='I am a tester.')
+
+        user2 = account_models.User.objects.create_user(email='mr@mypapaya.io', password='pass', username='aov_hovy')
+        profile2 = account_models.Profile.objects.create_or_update(user=user2, bio='Haha')
+
+        # Simulate auth
+        token = test_helpers.get_token_for_user(user)
+
+        # Get data from endpoint
+        client = APIClient()
+
+        payload = {
+            'id': profile2.id,
+            'bio': 'Foo'
+        }
+
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+
+        request = client.patch('/api/me/profile', data=payload, format='json')
+        result = request.data['result']
+
+        self.assertEquals(result['bio'], 'Foo')
+
+        # Check db entry
+        updated_profile = account_models.Profile.objects.get(user=user)
+
+        self.assertEquals(updated_profile.id, profile1.id)
