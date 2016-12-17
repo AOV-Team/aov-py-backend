e04ade2cfe0380847be12e24fc01dd826b81642f
Martin Ronquillo <ronquillo.m@gmail.com>
2016-12-17 02:51:03 -0700

    Added code to import feed data

--- apps/utils/management/commands/2016_migration.py
+++ apps/utils/management/commands/2016_migration.py
@@ -5,7 +5,6 @@ from apps.photo.photo import Photo
 from django.conf import settings
 from django.core.management import BaseCommand
 from django.db.utils import IntegrityError
-import collections
 import csv
 import datetime
 import os
@@ -32,60 +31,47 @@ def get_feed_data():
         for row in reader:
             feed_photos[row[1]] = row[0]
 
-    # ordered_feed_photos = collections.OrderedDict()
-    #
-    # for i in sorted(feed_photos.items(), key=lambda x: int(x[1])):
-    #     ordered_feed_photos[i[0]] = i[1]
-    #
-    # print(ordered_feed_photos)
-
     return feed_photos
 
 
-def import_users():
-    with open('user.csv', 'r') as csvfile:
+def import_feed():
+    """
+    Add images to feed. Run image import before running this file
+
+    :return: None
+    """
+    feed_images = list()
+
+    # Load data
+    with open('feed_images.csv', 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
 
         for row in reader:
-            # Handle age
-            try:
-                age = int(row[7])
-            except ValueError:
-                age = None
+            feed_images.append([int(row[0]), int(row[1])])
 
-            # Handle name
-            # Split by first space. First item is set as first name
-            # Subsequent items set as last name
-            name = row[1].split(' ')
-            first_name = name[0]
-            last_name = ''
+    # Sort images into proper order that they were added into the feed
+    feed_images = sorted(feed_images, key=lambda x: x[1])
 
-            if len(name) > 1:
-                counter = 1
+    # Add photos to AOV feed
+    feed = photo_models.PhotoFeed.objects.get(id=1)
 
-                while counter < len(name):
-                    last_name += name[counter] + ' '
-                    counter += 1
-
-            last_name = last_name.strip()
-
-            # Attempt to create user
-            # If username already exists, append a random string to end of username and create user
-            try:
-                account_models.User.objects\
-                    .create_user(age=age, created_at=row[8], email=row[2], first_name=first_name,
-                                 last_name=last_name, location=row[4], social_name=row[5], username=row[5])
-            except IntegrityError as e:
-                if '(username)' in str(e):
-                    account_models.User.objects \
-                        .create_user(age=age, created_at=row[8], email=row[2], first_name=first_name,
-                                     last_name=last_name, location=row[4], social_name=row[5],
-                                     username=row[5] + id_generator())
+    for image in feed_images:
+        photo = photo_models.Photo.objects.get(id=image[0])
+        photo.photo_feed = [feed]
+        photo.save()
 
 
 def import_images():
+    """
+    Import images from old DB
+
+    :return: None
+    """
     counter = 0
     already_processed = list()
+
+    # Get feed data
+    feed_data = get_feed_data()
 
     # See if there's a record file
     if os.path.isfile('processed_images.txt'):
@@ -144,16 +130,71 @@ def import_images():
                 f.write(image_file + '\n')
                 f.close()
 
+                # Save feed data for importing later if image is in feed
+                feed_index = feed_data[row[0]] if row[0] in feed_data else None
+
+                if feed_index:
+                    f = open('feed_images.csv', 'a+')
+                    # New photo id, feed index
+                    f.write('{},{}\n'.format(pic.id, feed_index))
+                    f.close()
+
                 counter += 1
 
     print('Imported {} images'.format(counter))
+
+
+def import_users():
+    """
+    Import users from old db
+
+    :return: None
+    """
+    with open('user.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+
+        for row in reader:
+            # Handle age
+            try:
+                age = int(row[7])
+            except ValueError:
+                age = None
+
+            # Handle name
+            # Split by first space. First item is set as first name
+            # Subsequent items set as last name
+            name = row[1].split(' ')
+            first_name = name[0]
+            last_name = ''
+
+            if len(name) > 1:
+                counter = 1
+
+                while counter < len(name):
+                    last_name += name[counter] + ' '
+                    counter += 1
+
+            last_name = last_name.strip()
+
+            # Attempt to create user
+            # If username already exists, append a random string to end of username and create user
+            try:
+                account_models.User.objects\
+                    .create_user(age=age, created_at=row[8], email=row[2], first_name=first_name,
+                                 last_name=last_name, location=row[4], social_name=row[5], username=row[5])
+            except IntegrityError as e:
+                if '(username)' in str(e):
+                    account_models.User.objects \
+                        .create_user(age=age, created_at=row[8], email=row[2], first_name=first_name,
+                                     last_name=last_name, location=row[4], social_name=row[5],
+                                     username=row[5] + id_generator())
 
 
 class Command(BaseCommand):
     help = 'Migrate from PHP backend to Django'
 
     def add_arguments(self, parser):
-        help_text = 'Name of action to perform (pp[prep photo data - merge feed and image files]|user|photo)'
+        help_text = 'Name of action to perform (feed|user|photo)'
 
         parser.add_argument('-a',
                             action='store',
@@ -167,11 +208,11 @@ class Command(BaseCommand):
 
         # Command expires on Dec 25, 2016
         if today <= 20161225:
-            if action == 'pp':
-                get_feed_data()
-            elif action == 'user':
+            if action == 'user':
                 import_users()
             elif action == 'photo':
                 import_images()
+            if action == 'feed':
+                import_feed()
             else:
                 print('Invalid command')
