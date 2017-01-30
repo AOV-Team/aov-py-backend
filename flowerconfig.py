140a7898c524d0375f84acf6c43d6ecfba409bcb
Martin Ronquillo <ronquillo.m@gmail.com>
2017-01-30 13:11:27 -0700

    Setting updates for celery and new Redis service

--- flowerconfig.py
+++ flowerconfig.py
@@ -1,3 +1,10 @@
 import os
 
-broker = 'redis://{}:{}/2'.format(os.environ.get('REDIS_HOST', '0.0.0.0'), os.environ.get('REDIS_PORT', 6379))
+password = ''
+redis_password = os.environ.get('REDIS_PASSWORD')
+
+if redis_password:
+    password = ':{}@'.format(redis_password)
+
+broker = 'redis://{}{}:{}/2'\
+    .format(password, os.environ.get('REDIS_HOST', '0.0.0.0'), os.environ.get('REDIS_PORT', 6379))
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,8 @@ if [ "$1" == "dev" ]; then
     cat > backend/settings/project_config.py <<'EOF'
 import os
 
+APNS_CERTIFICATE = 'aov_dev.pem'
+
 BROKER_URL = 'redis://{}:{}/2'.format(os.environ.get('REDIS_HOST', '0.0.0.0'), os.environ.get('REDIS_PORT', 6379))
 CELERY_RESULT_BACKEND = 'redis://{}:{}/3'.format(os.environ.get('REDIS_HOST', '0.0.0.0'), os.environ.get('REDIS_PORT', 6379))
 
@@ -62,8 +64,10 @@ elif [ "$1" == "production" ]; then
     cat > backend/settings/project_config.py <<'EOF'
 import os
 
-BROKER_URL = 'redis://{}:{}/2'.format(os.environ.get('REDIS_HOST', '0.0.0.0'), os.environ.get('REDIS_PORT', 6379))
-CELERY_RESULT_BACKEND = 'redis://{}:{}/3'.format(os.environ.get('REDIS_HOST', '0.0.0.0'), os.environ.get('REDIS_PORT', 6379))
+APNS_CERTIFICATE = os.environ.get('APNS_CERT')
+
+BROKER_URL = 'redis://:{}@{}:{}/2'.format(os.environ.get('REDIS_PASSWORD'), os.environ.get('REDIS_HOST'), os.environ.get('REDIS_PORT'))
+CELERY_RESULT_BACKEND = 'redis://:{}@{}:{}/3'.format(os.environ.get('REDIS_PASSWORD'), os.environ.get('REDIS_HOST'), os.environ.get('REDIS_PORT'))
 
 DATABASES = {
     'default': {
