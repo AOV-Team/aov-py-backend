e630ad7592601d0168276a396094800c41b5eff6
Martin Ronquillo <ronquillo.m@gmail.com>
2017-02-09 18:54:01 -0700

    Create device endpoint

--- apps/communication/test/test_devices_view_set.py
+++ apps/communication/test/test_devices_view_set.py
@@ -110,3 +110,57 @@ class TestDevicesViewSetGET(TestCase):
 
         self.assertEquals(len(results), 1)
         self.assertEquals(results[0]['id'], d2.id)
+
+
+class TestDevicesViewSetPOST(TestCase):
+    def test_devices_view_set_post_successful(self):
+        """
+        Test that we can save a device token for a user
+
+        :return: None
+        """
+        # Create test data
+        registration_id = '1D2440F1F1BB3C1D3953B40A85D02403726A48828ACF92EDD5F17AAFFBFA8B50'
+        user = User.objects.create_user('aov@aov.com', 'pass')
+
+        token = test_helpers.get_token_for_user(user)
+
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+
+        request = client.post('/api/devices', data={'registration_id': registration_id})
+
+        self.assertEquals(request.status_code, 201)
+
+        # query db
+        tokens = APNSDevice.objects.all()
+
+        self.assertEquals(tokens.count(), 1)
+
+        token = tokens.first()
+
+        self.assertEquals(token.user, user)
+        self.assertEquals(token.registration_id, registration_id)
+
+    def test_devices_view_set_post_missing_registration_id(self):
+        """
+        Test that we get HTTP 400 if registration_id is missing
+
+        :return: None
+        """
+        # Create test data
+        user = User.objects.create_user('aov@aov.com', 'pass')
+
+        token = test_helpers.get_token_for_user(user)
+
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+
+        request = client.post('/api/devices', data={})
+
+        self.assertEquals(request.status_code, 400)
+
+        # query db
+        tokens = APNSDevice.objects.all()
+
+        self.assertEquals(tokens.count(), 0)
--- apps/communication/views.py
+++ apps/communication/views.py
@@ -1,20 +1,22 @@
+from apps.common.views import get_default_response
 from apps.communication.serializers import AOVAPNSDeviceSerializer
-from apps.communication.models import PushMessage
+# from apps.communication.models import PushMessage
 from apps.communication.tasks import send_push_notification
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
 from django.contrib.admin.views.decorators import staff_member_required
 from django.db.models import Q
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from push_notifications.models import APNSDevice
 from rest_framework import generics
-from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAdminUser
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.permissions import IsAuthenticated
 
 
 class DevicesViewSet(generics.ListCreateAPIView):
-    authentication_classes = (SessionAuthentication,)
-    permission_classes = (IsAdminUser,)
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
     serializer_class = AOVAPNSDeviceSerializer
 
     def get_queryset(self):
@@ -23,6 +25,10 @@ class DevicesViewSet(generics.ListCreateAPIView):
 
         :return: Queryset
         """
+        # User needs to be an admin to query this endpoint
+        if not self.request.user.is_admin:
+            raise PermissionDenied('You must be an admin to access this data')
+
         query = self.request.query_params.get('q')
         queryset = APNSDevice.objects.all()
 
@@ -34,6 +40,34 @@ class DevicesViewSet(generics.ListCreateAPIView):
                         Q(user__username__icontains=query))
 
         return queryset.order_by('-date_created')
+
+    def post(self, request):
+        """
+        Create an APNS Device
+
+        :param request: Request object
+        :return: Response object
+        """
+        authentication = TokenAuthentication().authenticate(request)
+        authenticated_user = authentication[0] if authentication else request.user
+        payload = request.data
+
+        if 'registration_id' in payload:
+            data = {
+                'user': authenticated_user.id,
+                'registration_id': payload['registration_id']
+            }
+
+            serializer = AOVAPNSDeviceSerializer(data=data)
+
+            if serializer.is_valid():
+                serializer.save()
+
+                return get_default_response('201')
+            else:
+                raise ValidationError(serializer.errors)
+        else:
+            raise ValidationError('Missing required key "registration_id"')
 
 
 @staff_member_required
