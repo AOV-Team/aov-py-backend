4ac1175c37706cde803b04fab3382ce506c273bd
Garrett Allen <gallen@replypro.io>
2018-01-27 14:03:42 -0700

    Added a function to chunk the list of devices into segments of 100.

--- apps/communication/test/test_task_send_push_notification.py
+++ apps/communication/test/test_task_send_push_notification.py
@@ -33,24 +33,18 @@ class TestTaskSendPushNotification(TestCase):
         :return: None
         """
         # Create test data
-        used_ids = list()
-        for num in range(1, 10**6):
-            reg_id = ''.join(x.strip() for x in str(uuid.uuid4()).split('-'))
-            used_ids.append(reg_id)
-            APNSDevice.objects.create(registration_id=reg_id)
-            # APNSDevice.objects.create(registration_id='1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A')
+        APNSDevice.objects.create(registration_id='1D2440F1F1BB3C1D3953B40A85D02403726A48828ACF92EDD5F17AAFFBFA8B50')
+        APNSDevice.objects.create(registration_id='1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A')
 
         with mock.patch('push_notifications.apns.apns_send_bulk_message') as p:
             message = 'This is a test.'
 
             send_push_notification(message, 'all')
 
-            # p.assert_called_with(alert=message,
-            #                      registration_ids=[
-            #                          '1D2440F1F1BB3C1D3953B40A85D02403726A48828ACF92EDD5F17AAFFBFA8B50',
-            #                          '1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A'])
             p.assert_called_with(alert=message,
-                                 registration_ids=used_ids)
+                                 registration_ids=[
+                                     '1D2440F1F1BB3C1D3953B40A85D02403726A48828ACF92EDD5F17AAFFBFA8B50',
+                                     '1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A1A'])
 
     def test_task_send_push_notification_specific(self):
         """
