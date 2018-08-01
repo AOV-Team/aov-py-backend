dcfd78354bdfcdede3a79d58c74732411acae1a7
Garrett Allen <neelik.n33l@gmail.com>
2018-08-01 14:01:34 -0600

    Fixed Untracked Files

--- scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Starting all processes"
-echo "Must have virtual environment enabled"
-echo "FOR TESTING ONLY"
-echo
-flower -A backend.settings -p 5555 -conf=flowerconfig.py &
-echo
-celery -A backend.settings worker -B -l info &
